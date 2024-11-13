553cef22656a8267c225e049de3f2b9cc5f29fdc
Jun <kjoon418@naver.com>
2024-11-13 17:43:33 +0900

    Create classes associated with JWT and Spring Security
    reference: https://velog.io/@woogym/GDGoC-JWT%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84

---
+++ src/main/java/junwatson/mycreditcalculator/dto/token/TokenDto.java
@@ -0,0 +1,12 @@
+package junwatson.mycreditcalculator.dto.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenDto {
+    private String accessToken;
+}
