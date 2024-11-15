f3e53259512f6980078dd4079354516632ce6903
Jun <kjoon418@naver.com>
2024-11-15 15:18:55 +0900

    Use Enum converter of Spring

--- src/main/java/junwatson/mycreditcalculator/dto/request/LectureRegistrationRequestDto.java
+++ src/main/java/junwatson/mycreditcalculator/dto/request/LectureRegistrationRequestDto.java
@@ -13,28 +13,16 @@ public class LectureRegistrationRequestDto {
     private Double credit;
     private String major;
     private Integer semester;
-    private String type;
+    private LectureType type;
 
     public Lecture toEntityWithMember(Member member) {
-        LectureType lectureType;
-        switch (type) {
-            case "MajorCourses":
-                lectureType = LectureType.MajorCourses;
-                break;
-            case "GeneralEducationCourses":
-                lectureType = LectureType.GeneralEducationCourses;
-                break;
-            default:
-                throw new IllegalLectureTypeException("강의 등록 중 잘못된 LectureType 입력이 들어왔습니다.");
-        }
-
         return Lecture.builder()
                 .member(member)
                 .name(name)
                 .credit(credit)
                 .major(major)
                 .semester(semester)
-                .type(lectureType)
+                .type(type)
                 .build();
     }
 }
--- src/main/java/junwatson/mycreditcalculator/dto/request/LectureUpdateRequestDto.java
+++ src/main/java/junwatson/mycreditcalculator/dto/request/LectureUpdateRequestDto.java
@@ -12,21 +12,5 @@ public class LectureUpdateRequestDto {
     private Double credit;
     private String major;
     private Integer semester;
-    private String type;
-
-    public LectureType getType() {
-        LectureType lectureType;
-        switch (type) {
-            case "MajorCourses":
-                lectureType = LectureType.MajorCourses;
-                break;
-            case "GeneralEducationCourses":
-                lectureType = LectureType.GeneralEducationCourses;
-                break;
-            default:
-                throw new IllegalLectureTypeException("강의 등록 중 잘못된 LectureType 입력이 들어왔습니다.");
-        }
-
-        return lectureType;
-    }
+    private LectureType type;
 }
